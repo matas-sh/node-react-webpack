ff66c91b22366e280f60cba77f1d3d9c25ea7bce
mataxxx5 <m.bartosevicius@gmail.com>
2020-01-30 15:02:47 +0000

    should fail

--- tests/simpleAssert.js
+++ tests/simpleAssert.js
@@ -10,12 +10,8 @@ it('test pass', () => {
     expect(testPass).to.equal(testString);
 });
 
-/*
-
-    Testing if passing all tests allows a merge
-
 it('test pass', () => {
     // should fail - should prevent from merging a pull request +
     expect(testFail).to.equal(testString);
 });
-*/
+
