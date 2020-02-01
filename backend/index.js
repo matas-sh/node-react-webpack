53c88c3f321186632bb39690e111f7746a2ced24
mataxxx5 <m.bartosevicius@gmail.com>
2020-02-01 14:08:42 +0000

    set up testing framework for frotend, changed test runner fro backend, fixed yml

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,6 @@ const HTML_ROOT_PATH = path.join(__dirname, './../frontend/index.html');
 app.use(express.static(path.resolve(BUILD_DIR)));
 
 app.get('*', (req, res) => {
-    console.log('ping');
-    console.log('BUILD_DIR: ', BUILD_DIR);
     res.sendFile(HTML_ROOT_PATH)
 });
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './logo.jpg';
 import '../style/app.scss';
 
-console.log('why though?');
 function App () {
     return (
         <div className="App">
