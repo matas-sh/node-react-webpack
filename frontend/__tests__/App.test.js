255dd147bff53bd1809e5610010893521c46d17d
mataxxx5 <m.bartosevicius@gmail.com>
2020-02-01 16:42:58 +0000

    configured eslint with airbnb rules

--- frontend/__tests__/App.test.js
+++ frontend/__tests__/App.test.js
@@ -4,7 +4,6 @@ import App from '../src/App';
  * Simple test for checking if App structure is still the same
  */
 it('testing App with Jest snapshot testing', () => {
-    
-    const renderedApp = App();
-    expect(renderedApp).toMatchSnapshot();
+  const renderedApp = App();
+  expect(renderedApp).toMatchSnapshot();
 });
