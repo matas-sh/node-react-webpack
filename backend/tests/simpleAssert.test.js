2db682a6999c0121cbeadeefbc1b95c42e058a9c
mataxxx5 <m.bartosevicius@gmail.com>
2020-02-01 20:53:44 +0000

    test test fail

--- backend/tests/simpleAssert.test.js
+++ backend/tests/simpleAssert.test.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 let testString = 'pass';
-let testPass = 'pass';
+let testPass = 'fail';
 
 // simple assert test to test jest + chai
 it('test pass', () => {
